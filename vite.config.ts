358d640a17a1fabc786a10d96f0f3993d4d65b46
Daniel Infante <danielinfantito61@gmail.com>
2025-05-17 16:24:06 -0500

    ci: deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/tenpo-auth',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
